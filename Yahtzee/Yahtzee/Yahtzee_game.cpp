2bd77d2bfa5b06ccebf9cc5a8283ede156bdfaae
laruchsix <61274375+laruchsix@users.noreply.github.com>
2021-12-05 17:58:10 +0100

    yahtzee_game

--- Yahtzee/Yahtzee/Yahtzee_game.cpp
+++ Yahtzee/Yahtzee/Yahtzee_game.cpp
@@ -1,7 +1,5 @@
 #include "Yahtzee_game.h"
 
-// TODO forme canonique
-
 // constructeur ou on connais deja le nom de joureur a l'avance
 Yahtzee_game::Yahtzee_game(int nb_joueurs, std::string nom_joueurs[],
 	int nb_lancer_par_tours) : nb_joueur(nb_joueurs), nb_lancer(nb_lancer_par_tours)
@@ -14,7 +12,6 @@ Yahtzee_game::Yahtzee_game(int nb_joueurs, std::string nom_joueurs[],
 	numero_tour = 0;
 	lancer = new Lancer;
 }
-
 
 // constructeur on ne connais pas le nom des joureur, un scanner le demande le nombre de joueur pour la partie et leur nom
 Yahtzee_game::Yahtzee_game(int nb_lancer_par_tours) : nb_lancer(nb_lancer_par_tours)
@@ -41,6 +38,18 @@ Yahtzee_game::Yahtzee_game(int nb_lancer_par_tours) : nb_lancer(nb_lancer_par_to
 	lancer = new Lancer;
 }
 
+Yahtzee_game::Yahtzee_game(const Yahtzee_game& copy)
+	: nb_joueur(copy.nb_joueur), nb_lancer(copy.nb_lancer)
+{
+	joueurs = new Joueur * [nb_joueur];
+
+	for (int i = 0; i < nb_joueur; i++) {
+		joueurs[i] = new Joueur(this, copy.joueurs[i].get_nom());
+	}
+	numero_tour = copy.numero_tour;
+	lancer = new Lancer;
+}
+
 // destructeur
 Yahtzee_game::~Yahtzee_game()
 {
@@ -54,11 +63,38 @@ Yahtzee_game::~Yahtzee_game()
 	delete lancer; 
 }
 
+Yahtzee_game& Yahtzee_game::operator=(const Yahtzee_game& copy)
+{
+	if (this != &copy) {
+		// destruction de tous les joueur pour éviter la fuite mémoire
+		for (int i = 0; i < nb_joueur; i++) {
+			delete joueurs[i];
+		}
+		delete[] joueurs;
+
+		// copy des joueurs et des propriétés
+		nb_joueur = copy.nb_joueur;
+		nb_lancer = copy.nb_lancer;
+		numero_tour = copy.numero_tour;
+		joueurs = new Joueur * [nb_joueur];
+
+		for (int i = 0; i < nb_joueur; i++) {
+			joueurs[i] = new Joueur(*copy.joueurs[i]);
+		}
+
+		// on ne copie pas lancer car elle son contenue nous interesse pas TODO
+	}
+
+	return *this;
+}
+
 void Yahtzee_game::jouer()
 {
+	// faire tous les tours
 	for (numero_tour = 1; numero_tour <= 13; numero_tour++) {
+		// chaque joueur joue
 		for (int index_joueur = 0; index_joueur < nb_joueur; index_joueur++) {
-
+			joueurs[index_joueur]->tour_joueur(lancer);
 		}
 	}
 }
--- Yahtzee/Yahtzee/Yahtzee_game.h
+++ Yahtzee/Yahtzee/Yahtzee_game.h
@@ -6,17 +6,21 @@
 #include "Joueur.h"
 #include "Lancer.h"
 
+const int NB_DE = 5; 
+
 class Yahtzee_game
 {
-    const int nb_lancer;
+    int nb_lancer;
     Joueur** joueurs;
     int nb_joueur;
     int numero_tour;
     Lancer* lancer;
 public:
     Yahtzee_game(int nb_joueurs, std::string nom_joueurs[], int nb_lancer_par_tours);
-    Yahtzee_game(int nb_lancer_par_tours);
-    ~Yahtzee_game();
+    Yahtzee_game(int nb_lancer_par_tours); // constructeur
+    Yahtzee_game(const Yahtzee_game& copy); // constructeur par copy
+    ~Yahtzee_game(); // desctructeur
+    Yahtzee_game& operator=(const Yahtzee_game& copy); // 
     void jouer();
 };
 #endif
