7732b224f8c7c493d17b6bb2d17abf88795168a6
Rémi Orveau <78747414+Bleomoon@users.noreply.github.com>
2021-12-08 20:22:28 +0100

    oui

---
+++ Yahtzee/test_Yahtzee/test_Yahtzee.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testYahtzee
+{
+	TEST_CLASS(testYahtzee)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
